d02d41929593b883bdc0a4986f9f04662453a9d0
Marco Gallegos <ma.galle.loae@gmail.com>
2019-10-22 17:50:22 -0500

    update : temp
    se esperan cambios de luis manuel, se añade solo un filtro a la
    consullta en api/app.py

--- api/app.py
+++ api/app.py
@@ -16,8 +16,12 @@ if __name__ == '__main__':
 
     try :
         Servidores.create(direccion="127.0.0.1", alias_servidor="marco " + str(date.today()), birthday=date(1940, 1, 1)
-                      , status="F")
+                      , status="I")
     except DatabaseError as e:
         print(e)
+
+    for serv in Servidores.select().where(Servidores.status == "A"):
+        print(f"servidor {serv.alias_servidor} status {serv.status}")
+
     app.run(debug=True, host='127.0.0.1', port=3000)
 
