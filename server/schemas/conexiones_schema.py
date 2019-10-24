658275d4f10c9fd7dfcd72dfcb296dfa2f34b221
Marco Gallegos <ma.galle.loae@gmail.com>
2019-10-24 17:14:53 -0500

    update : crud conexiones
    previo de el crud de conexiones, faltan validaciones

---
+++ server/schemas/conexiones_schema.py
@@ -0,0 +1,93 @@
+from models.conexiones import Conexiones
+import graphene
+
+# usado para definir los datos que se recuperan de un objeto(modelo peewee) tpo Servidores por GQL
+class ConexionesSchema(graphene.ObjectType):
+    id_conexion = graphene.Int()
+    id_servidor = graphene.Int()
+    id_conector = graphene.Int()
+    puerto = graphene.String()
+    usuario = graphene.String()
+    contrasena = graphene.String()
+
+
+# usado para recibir un objeto tipo servidor como parametro en graphql
+class ConexionesInput(graphene.InputObjectType):
+    id_servidor = graphene.Int(required=True)
+    id_conector = graphene.Int(required=True)
+    puerto = graphene.String(required=True)
+    usuario = graphene.String(required=True)
+    contrasena = graphene.String(required=True)
+
+
+class ConexionesQuery(graphene.ObjectType):
+    conexiones = graphene.List(ConexionesSchema)
+    conexion = graphene.Field(ConexionesSchema, id_conexion=graphene.Int(required=True))
+
+    @staticmethod
+    def resolve_conexiones(self, info):
+        result = Conexiones.select()
+        return result
+
+    @staticmethod
+    def resolve_conexion(self, info, id_conexion):
+        result = Conexiones.select().where(Conexiones.id_servidor == id_conexion).get()
+        return result
+
+
+# definimos mutacion para  crear, editar y eliminar
+class ConexionesCreateMutation(graphene.Mutation):
+    class Arguments:
+        id_servidor = graphene.Int(required=True)
+        id_conector = graphene.Int(required=True)
+        puerto = graphene.String(required=True)
+        usuario = graphene.String(required=True)
+        contrasena = graphene.String(required=True)
+
+    conexion = graphene.Field(ConexionesSchema)
+
+    @staticmethod
+    def mutate(self, info, id_servidor, id_conector, puerto, usuario, contrasena):
+        modelo = Conexiones(id_servidor=id_servidor, id_conector=id_conector, puerto=puerto, usuario=usuario, contrasena=contrasena)
+        modelo.save()
+        return ConexionesCreateMutation(conexion=modelo)
+
+
+class ConexionesDeleteMutation(graphene.Mutation):
+    class Arguments:
+        id_conexion=graphene.Int(required=True)
+
+    conexion = graphene.Field(ConexionesSchema)
+
+    @staticmethod
+    def mutate(self, info, id_conexion):
+        modelo = Conexiones.select().where(Conexiones.id_conexion == id_conexion).get()
+        #servidor.status = "E"
+        #servidor.save()
+        return ServidoresDeleteMutation(conexion=modelo)
+
+
+class ConexionesUpdateMutation(graphene.Mutation):
+    class Arguments:
+        id_conexion = graphene.Int(required=True)
+        conexion_data = ConexionesInput(required=True)
+
+    conexion = graphene.Field(ConexionesSchema)
+
+    @staticmethod
+    def mutate(self, info, id_conexion, conexion_data):
+        modelo = Conexiones.select().where(Conexiones.id_conexion == id_conexion).get()
+        #servidor.direccion = servidor_data.direccion
+        #servidor.alias_servidor = servidor_data.alias_servidor
+        #servidor.status = servidor_data.status
+        #servidor.save()
+        return ConexionesUpdateMutation(conexion=modelo)
+
+
+class ConexionesMutations(graphene.ObjectType):
+    createConexion = ConexionesCreateMutation.Field()
+    deleteConexion = ConexionesDeleteMutation.Field()
+    updateConexion = ConexionesUpdateMutation.Field()
+
+
+SchemaConexiones = graphene.Schema(query=ConexionesQuery, mutation=ConexionesMutations)
