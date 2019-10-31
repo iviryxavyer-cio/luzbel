38972d32e6b14a7578bee21d9a00789b139f14ad
Camarada Treviño <luistorres.compv79@gmail.com>
2019-10-31 15:07:17 -0600

    Documentacion en el archivo de acciones

--- app/alloces/src/actions/login.actions.js
+++ app/alloces/src/actions/login.actions.js
@@ -1,36 +1,58 @@
-import {loginConstants} from '../constants/login.constants';
+import { loginConstants } from '../constants/login.constants';
 import { LoginService } from '../services/login.service';
 
 export const loginActions = {
     login
 }
 
+/**
+ * Esta funcion se encarga de realizar el login con el servidor de la api consumiendo 
+ * el servicio de loginService.
+ * @param {string} username nombre de usuario en el sistema
+ * @param {string} password contraseña del usuario en el sistema
+ */
 function login(username, password) {
     return dispatch => {
         //dispara la accion de la peticion
         dispatch(request())
 
+        //Segmento Try/Catch que ejecuta el servicio de login.
         try {
+            //Se usa el metodo login del loginService pasandole las credenciales desde el formulario
             LoginService.login(username, password).then((token) => {
-                console.log(token)
-            if(token){
-                localStorage.setItem('token', token.loginUsuario.token)
-                dispatch(success(token.loginUsuario.token))
-            }
-            }).catch((e)=>{
-                dispatch(fail())
+
+                //Si la el servicio no tuvo errores entonces guarda el token en el localStorage y hace un dispatch
+                //a la accion de success donde se le pasa el el token de usuario
+                if (token) {
+                    localStorage.setItem('token', token.loginUsuario.token)
+                    dispatch(success(token.loginUsuario.token))
+                }
             })
-        }catch(e){
+            //Si hay un error, hace un dispatch con la funcion de error
+        } catch (e) {
             dispatch(fail())
         }
     }
 
-    function request(){return {type: loginConstants.LOGIN_USER_REQUEST}}
-    function success(token){
+    /**
+     * funcion request
+     * regresa un objeto para el dispatch request
+     */
+    function request() { return { type: loginConstants.LOGIN_USER_REQUEST } }
+    /**
+     * funcion success
+     * regresa un objeto con el typo de reduce de success y un payload con la cadena de token
+     * @param {string} token Cadena de token de usuario
+     */
+    function success(token) {
         return {
             type: loginConstants.LOGIN_USER_REQUEST_SUCCESS,
             payload: token
         }
     }
-    function fail(){return { type: loginConstants.LOGIN_USER_REQUEST_FAIL }}
+    /**
+     * funcion fail
+     * regresa un objeto con el tipo de reduce de fail
+     */
+    function fail() { return { type: loginConstants.LOGIN_USER_REQUEST_FAIL } }
 }
