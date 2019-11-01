3de9623c0120b18867031a6fd68d1407a6e98999
Camarada Treviño <luistorres.compv79@gmail.com>
2019-11-01 11:37:56 -0600

    Resolucion de conflictos con la version de luzbel

--- app/alloces/src/index.js
+++ app/alloces/src/index.js
@@ -11,11 +11,9 @@ import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 import { Provider } from 'react-redux';
-import { store } from './store/index';
-
-import { Provider } from 'react-redux';
 
 import {store} from './store';
+
 
 ReactDOM.render(
     <Provider store={store}><App /></Provider>
