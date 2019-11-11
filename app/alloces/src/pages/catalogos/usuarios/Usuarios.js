c3844846a0cacb7d6d69400998885d28c55ea596
Luis Manuel Torres Treviño <cain_1@ciosa.com>
2019-11-11 12:26:21 -0600

    Se creo nueva carpeta para los catalogos

--- app/alloces/src/pages/catalogos/usuarios/Usuarios.js
+++ app/alloces/src/pages/catalogos/usuarios/Usuarios.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import '../App.css'
+import '../../../App.css'
 import { Col } from 'reactstrap'
 import { ButtonToolbar, Button } from 'react-bootstrap'
-import DataTableUsuarios from '../componentes/DataTableUsuarios'
+import DataTableUsuarios from '../../../componentes/DataTableUsuarios'
 
 function Usuarios(){
     
