16c0d20874ef41a19195d7f61e8d15b762434e04
Luis Manuel Torres Treviño <cain_1@ciosa.com>
2019-11-17 12:55:13 -0600

    Se añadieron nuevas constantes al archivo de drivers

--- app/alloces/src/constants/drivers.constants.js
+++ app/alloces/src/constants/drivers.constants.js
@@ -1,5 +1,55 @@
 export const DriversConstants = {
     GET_ALL_DRIVERS_REQUEST: 'GET_ALL_DRIVERS_REQUEST',
     GET_ALL_DRIVERS_REQUEST_SUCCESS: 'GET_ALL_DRIVERS_REQUEST_SUCCESS',
-    GET_ALL_DRIVERS_REQUEST_FAIL: 'GET_ALL_DRIVERS_REQUEST_FAIL'
+    GET_ALL_DRIVERS_REQUEST_FAIL: 'GET_ALL_DRIVERS_REQUEST_FAIL',
+
+    CREAR_CONECTORES_REQUEST: 'CREAR_CONECTORES_REQUEST',
+    CREAR_CONECTORES_EXITO: 'CREAR_CONECTORES_EXITO',
+    CREAR_CONECTORES_FRACASO: 'CREAR_CONECTORES_FRACASO',
+
+    MODIFICAR_CONECTORES_REQUEST: 'MODIFICAR_CONECTORES_REQUEST',
+    MODIFICAR_CONECTORES_EXITO: 'MODIFICAR_CONECTORES_EXITO',
+    MODIFICAR_CONECTORES_FRACASO: 'MODIFICAR_CONECTORES_FRACASO',
+
+    ELIMINAR_CONECTORES_REQUEST: 'ELIMINAR_CONECTORES_REQUEST',
+    ELIMINAR_CONECTORES_EXITO: 'ELIMINAR_CONECTORES_EXITO',
+    ELIMINAR_CONECTORES_FRACASO: 'ELIMINAR_CONECTORES_FRACASO',
+
+    URL_CONECTORES_GRAPHQL: 'http://localhost/conector',
+
+    QUERY_OBTENER_CONECTORES: `{
+        conectores {
+            idConector
+            nombreConector
+            urlConector
+            status
+        }
+    }`,
+
+    QUERY_CREAR_CONECTORES: `
+        mutation CrearConector($nombreConector: String!, $urlConector: String!){
+            crearConector(nombreConector: $nombreConector urlConector: $urlConector){
+                conector {
+                    idConector
+                }
+            }
+        }
+    `,
+
+    //TODO: terminar modificar conector
+    QUERY_MODIFICAR_CONECTORES: `
+        mutation ActualizarConector($idConector: Int!){
+            
+        }
+    `,
+
+    QUERY_ELIMINAR_CONECTORES:`
+        mutation BorrarConector($idConector: Int!){
+            borrarConector(idConector: $idConector){
+                conector{
+                    idConector
+                }
+            }
+        }
+    `
 }
