862b6b56051bf9c1c98d84a623b7d7136aa97437
Luis Manuel Torres Treviño <cain_1@ciosa.com>
2019-11-24 21:39:05 -0600

    Se termino el menu lateral creado desde un array

--- app/alloces/src/componentes/comun/sideBar.component.js
+++ app/alloces/src/componentes/comun/sideBar.component.js
@@ -8,7 +8,7 @@
  * v.1.0.0 - Esta version del archivo es el que se encarga de crear el menu sidebar
  */
 import React from 'react';
-import { makeStyles, createStyles } from '@material-ui/core/styles';
+import _ from 'lodash';
 
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -16,15 +16,9 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
 import Collapse from '@material-ui/core/Collapse';
+import { Link } from 'react-router-dom';
 
-import IconExpandLess from '@material-ui/icons/ExpandLess';
-import IconExpandMore from '@material-ui/icons/ExpandMore';
-import IconDashboard from '@material-ui/icons/Dashboard';
-import IconShoppingCart from '@material-ui/icons/ShoppingCart';
-import IconPeople from '@material-ui/icons/People';
-import IconBarChart from '@material-ui/icons/BarChart';
-import IconLibraryBooks from '@material-ui/icons/LibraryBooks';
-
+import { menuPrivado } from '../../plantilla/privado/menuPrivado';
 import '../../css/sidebar.scss';
 
 const SideBar = () => {
@@ -33,27 +27,48 @@ const SideBar = () => {
     const handleClick = () => {
         setOpen(!open)
     }
-
     return (
         <List component="nav" className="appMenu" disablePadding>
-            <ListItem button onClick={handleClick} className="menuItem">
-                <ListItemIcon className="menuItemIcon">
-                    <IconLibraryBooks className="colorIcon"/>
-                </ListItemIcon>
-                <ListItemText primary="Catalogos" className="textColor"/>
-                {open ? 
-                    <IconExpandLess className="secondaryIcon" /> : 
-                    <IconExpandMore className="secondaryIcon" />
-                }
-            </ListItem>
-            <Collapse in={open} timeout="auto" unmountOnExit>
-                <Divider />
-                <List component="div" disablePadding>
-                    <ListItem button className="menuItem">
-                        <ListItemText inset primary="Usuarios" className="textColor" />
+            {menuPrivado.map((element, key) => {
+                return (
+                element.collapse ? 
+                    <>
+                    <ListItem button onClick={handleClick} className="menuItem">
+                        <ListItemIcon className="menuItemIcon">
+                            {element.icon}
+                        </ListItemIcon>
+                        <ListItemText primary={element.name} className="textColor" />
+                        {open ?
+                            element.openIcon : element.closeIcon
+                        }
                     </ListItem>
-                </List>
-            </Collapse>
+                    <Collapse in={open} timeout="auto" unmountOnExit>
+                        <Divider />
+                        <List component="div" disablePadding>
+                            {element.collapseItems.map((innerElement, innerKey) => {
+                                return (
+                                    <Link to={innerElement.link}>
+                                        <ListItemText inset primary={innerElement.name} className="textColor" />
+                                    </Link>
+                                )
+                            })}
+                            <Divider />
+                        </List>
+                    </Collapse>
+                    <Divider />
+                    </>
+                :
+                    <>
+                        <ListItem button className="menuItem">
+                            <ListItemIcon className="menuItemIcon">
+                                {element.icon}
+                            </ListItemIcon>
+                            <ListItemText primary={element.name} className="textColor" />
+                        </ListItem>
+                        <Divider />
+                    </>
+            )})
+            }
         </List>
     )
 }
---
+++ app/alloces/src/plantilla/privado/menuPrivado.js
@@ -0,0 +1,36 @@
+/**
+ * @author Luis Manuel Torres Treviño
+ * @date 23/11/2019
+ * @fileoverview Este archivo contiene el objeto con el cual se creará el menu lateral
+ * @version 1.0.0
+ * 
+ * Historial
+ * v.1.0.0 Se crea el menu de catalogos con usuarios, servidores, conectores y conexiones.
+ */
+
+import React from 'react';
+import IconLibraryBooks from '@material-ui/icons/LibraryBooks';
+import IconExpandLess from '@material-ui/icons/ExpandLess';
+import IconExpandMore from '@material-ui/icons/ExpandMore';
+
+export const menuPrivado = [
+    {
+        name: 'Catalogos',
+        icon: <IconLibraryBooks className="colorIcon" />,
+        openIcon: <IconExpandLess className="secondaryIcon" />,
+        closeIcon: <IconExpandMore className="secondaryIcon" />,
+        collapse: true,
+        collapseItems: [
+            {
+                name: 'Usuarios',
+                link: '/usuarios',
+            },
+            {
+                name: 'Servidores',
+                link: '/servidores'
+            },
+            { name: 'Conectores', link: '/conectores' },
+            { name: 'Conexiones', link: '/conexiones' },
+        ]
+    },
+]
