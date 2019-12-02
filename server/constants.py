0b3a731305be3fc066a60565b0f54f68f85dd1b1
Luis Manuel Torres Treviño <cain_1@ciosa.com>
2019-12-02 10:49:54 -0600

    [ADD] se añade nuevo metodo en la utilerias get_columns para obtener una lista con las columnas de un tabla

--- server/constants.py
+++ server/constants.py
@@ -7,3 +7,6 @@ LOGGER_DIR = os.path.join(APP_DIR, '/logs')
 
 # mensajes
 ERROR_GRAPHQL = 'Error con la mutacion de graphql'
+ERROR_PYMSSQL_20009 = 'conexion rechazada, revisar direccion y puerto del servidor.'
+ERROR_PYMSSQL_18456 = 'conexion rechazada, revisar usuario, contraseña o base de datos.'
+ERROR_PYMSSQL_DEFAULT = 'error de conexion no clasificado: {}.'
