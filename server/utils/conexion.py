5e1e3a65ea880f6e4aa6201696f2be70c171e0e6
Luis Manuel Torres Trevino <torrestrevino..luis@gmail.com>
2020-02-14 14:56:10 -0600

    [IMP] cambios en la conexion de la base de datos

--- server/utils/conexion.py
+++ server/utils/conexion.py
@@ -1,7 +1,8 @@
 from peewee import PostgresqlDatabase
 
-# psql_db = PostgresqlDatabase('necronomicon', user="postgres", password="postgres", host="10.1.8.8")
 # psql_db = PostgresqlDatabase(
-#    'necronomicon', user="postgres", password="postgres", host="localhost", port=5432)
+#    'necronomicon', user="postgres", password="postgres", host="10.1.8.8")
 psql_db = PostgresqlDatabase(
-    'necronomicon', user="postgres", password="postgres", host="postgres", port=5432)
+    'necronomicon', user="postgres", password="postgres", host="localhost", port=5432)
+# psql_db = PostgresqlDatabase(
+#    'necronomicon', user="postgres", password="postgres", host="postgres", port=5432)
